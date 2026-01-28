85169f60d129881617de3ec8703885a692c46eac
Riz <ryzmdn@gmail.com>
2026-01-28 08:03:55 +0700

    feat: added a contact page

--- app/kontak/page.tsx
+++ app/kontak/page.tsx
@@ -1,3 +1,25 @@
+import { Container } from "@/components/container";
+import { SectionHeader } from "@/components/heading";
+import { ContactCard } from "@/components/ui/contact-card";
+import { contacts } from "@/constants";
+
 export default function Page() {
-  return <h1>Hello World</h1>;
+  return (
+    <Container
+      as="section"
+      className="space-y-12 px-4 my-12 py-20 sm:px-6 lg:px-8"
+    >
+      <SectionHeader
+        eyebrow="Layanan Bantuan"
+        title="Kontak Pengelola Lab"
+        description="Hubungi tim teknis atau administrasi kami untuk informasi terkait peminjaman ruang, kendala fasilitas, atau konsultasi kegiatan praktikum."
+      />
+
+      <ul className="mx-auto grid max-w-3xl grid-cols-1 gap-5 sm:grid-cols-2">
+        {contacts.map((item) => (
+          <ContactCard key={item.title} {...item} />
+        ))}
+      </ul>
+    </Container>
+  );
 }
