66bdb8332c62c67ca971927d9b8140cd3a035ced
Riz <ryzmdn@gmail.com>
2026-01-30 08:26:31 +0700

    style(course): adjusted the course page layout

--- components/ui/course-card.tsx
+++ components/ui/course-card.tsx
@@ -9,12 +9,12 @@ export function CourseCard({
 }: Readonly<Course>) {
   return (
     <div className="size-full p-6 bg-base-50 border border-base-200 rounded-2xl">
-      <div className="flex items-center sm:justify-between">
+      <div className="flex justify-between items-center w-full">
         <span className="inline-flex items-center rounded-full bg-orange-50 size-14 p-3 text-orange-700 inset-ring inset-ring-orange-600/10 dark:bg-orange-400/10 dark:text-orange-400 dark:inset-ring-orange-400/20">
           <Icon className="size-full" />
         </span>
 
-        <div className="hidden flex-col items-end gap-y-1 sm:flex">
+        <div className="flex flex-col items-end gap-y-1">
           <span className="inline-flex items-center rounded-full bg-amber-50 px-2 py-1 text-xs font-medium text-amber-700 inset-ring inset-ring-amber-600/20 dark:bg-amber-400/10 dark:text-amber-400 dark:inset-ring-amber-500/20">
             Semester {semester}
           </span>
@@ -29,15 +29,6 @@ export function CourseCard({
           {name}
         </h3>
         <p className="text-base/7 text-base-foreground-400">{description}</p>
-
-        <div className="flex gap-x-1 mt-5 sm:hidden">
-          <span className="inline-flex items-center w-max rounded-full bg-orange-50 px-2 py-1 text-xs font-medium text-orange-600 inset-ring inset-ring-orange-500/10">
-            Semester {semester}
-          </span>
-          <span className="inline-flex items-center w-max rounded-full bg-amber-50 px-2 py-1 text-xs font-medium text-amber-700 inset-ring inset-ring-amber-600/10">
-            SKS {sks}
-          </span>
-        </div>
       </div>
     </div>
   );
