a69b718fd2b0d31e308515ee0d6198863600c3d1
Riz <ryzmdn@gmail.com>
2026-02-01 00:25:33 +0700

    style(contact-card): contact card color and layout adjustment

--- components/home/contact-section.tsx
+++ components/home/contact-section.tsx
@@ -4,12 +4,12 @@ import { contacts } from "@/constants";
 
 export function ContactSection() {
   return (
-    <section className="space-y-12 px-4 mb-10 pb-10 py-14 sm:px-6 lg:px-8">
+    <Container as="section" className="space-y-12 px-4 mb-10 pb-10 py-14 sm:px-6 lg:px-8">
       <ul className="mx-auto grid max-w-3xl grid-cols-1 gap-5 sm:grid-cols-2">
         {contacts.map((item) => (
           <ContactCard key={item.title} {...item} />
         ))}
       </ul>
-    </section>
+    </Container>
   );
 }
