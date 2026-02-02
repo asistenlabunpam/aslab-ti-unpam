901f2b39fc9ebbb2c8f51ae9f56cadf057d9e10a
Riz <ryzmdn@gmail.com>
2026-02-02 16:44:59 +0700

    feat(activity-animate): added activity page animations

--- app/kegiatan/page.tsx
+++ app/kegiatan/page.tsx
@@ -1,6 +1,7 @@
 import { Container } from "@/components/container";
 import { SectionHeader } from "@/components/heading";
 import { Card } from "@/components/ui/apple-cards-carousel";
+import { BlurFade } from "@/components/ui/blur-fade";
 import { data } from "@/constants";
 
 export default function Page() {
@@ -20,13 +21,14 @@ export default function Page() {
         className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3"
       >
         {data.map((card, index) => (
-          <Card
-            key={card.src}
-            card={card}
-            index={index}
-            defaultClass={false}
-            className="relative group flex h-96 w-auto max-w-sm mx-auto flex-col items-start justify-start overflow-hidden p-2 rounded-xl bg-base-100 sm:max-w-none sm:mx-0 md:h-128 md:rounded-3xl"
-          />
+          <BlurFade key={card.title} delay={0.5 + index * 0.15} direction="up" inView>
+            <Card
+              card={card}
+              index={index}
+              defaultClass={false}
+              className="relative group flex h-96 w-auto max-w-sm mx-auto flex-col items-start justify-start overflow-hidden p-2 rounded-xl bg-base-100 sm:max-w-none sm:mx-0 md:h-128 md:rounded-3xl"
+            />
+          </BlurFade>
         ))}
       </Container>
     </Container>
