e5275ae51b4e3852403daebbee327c4655e79378
Riz <ryzmdn@gmail.com>
2026-02-02 13:50:11 +0700

    feat(animate): added motion animation to each item

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,31 +1,70 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
+import { motion, Variants } from "motion/react";
 
 export default function NotFound() {
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const variant: Variants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.5, ease: "easeOut" } },
+  };
+
   return (
-    <div className="flex flex-col justify-center items-center min-h-dvh text-center px-6 py-24 sm:py-32 lg:px-8">
-      <p className="text-8xl font-semibold bg-linear-to-r bg-clip-text text-transparent from-sky-500 to-blue-500 sm:text-9xl">
+    <motion.div
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      className="flex flex-col justify-center items-center min-h-dvh text-center px-6 py-24 sm:py-32 lg:px-8"
+    >
+      <motion.p 
+        variants={variant}
+        className="text-8xl font-semibold bg-linear-to-r bg-clip-text text-transparent from-sky-500 to-blue-500 sm:text-9xl"
+      >
         Oops!
-      </p>
-      <h2 className="scroll-m-20 text-center text-3xl font-semibold tracking-tight text-balance mt-10 mb-5 sm:text-4xl">
+      </motion.p>
+
+      <motion.h2 
+        variants={variant}
+        className="scroll-m-20 text-center text-3xl font-semibold tracking-tight text-balance mt-10 mb-5 sm:text-4xl"
+      >
         Sepertinya Kamu Tersesat
-      </h2>
-      <p className="text-base/7 text-pretty text-base-foreground-400 sm:text-xl/relaxed">
+      </motion.h2>
+
+      <motion.p 
+        variants={variant}
+        className="text-base/7 text-pretty text-base-foreground-400 sm:text-xl/relaxed max-w-2xl"
+      >
         Halaman yang kamu cari tidak ada di sini. Mungkin sudah pindah atau
         tautannya salah ketik.
-      </p>
+      </motion.p>
 
-      <div className="mt-10 flex items-center justify-center gap-x-6">
-        <Button size="lg">
+      <motion.div 
+        variants={variant}
+        className="mt-10 flex items-center justify-center gap-x-4"
+      >
+        <Button size="lg" asChild>
           <Link href="/">Kembali</Link>
         </Button>
-        <Button size="lg" variant="ghost" className="group">
+
+        <Button size="lg" variant="ghost" className="group" asChild>
           <Link href="/kontak" className="flex items-center gap-x-1.5">
-            Bantuan <ArrowRight className="size-3 group-hover:translate-x-1" />
+            Bantuan 
+            <ArrowRight className="size-4 transition-transform duration-200 group-hover:translate-x-1" />
           </Link>
         </Button>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 }
