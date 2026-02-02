1166f57412bc64f4e5e9ba4680ec6c059efec0ee
Riz <ryzmdn@gmail.com>
2026-02-02 16:51:22 +0700

    feat(recruitment-animate): added recruitment page animation

--- app/(informasi)/recruitment/page.tsx
+++ app/(informasi)/recruitment/page.tsx
@@ -1,5 +1,6 @@
 import { Container } from "@/components/container";
 import { SectionHeader } from "@/components/heading";
+import { BlurFade } from "@/components/ui/blur-fade";
 import { Button } from "@/components/ui/button";
 import { RecruitmentCard } from "@/components/ui/recruitment-card";
 import { Separator } from "@/components/ui/separator";
@@ -30,11 +31,14 @@ export const recruitment: RecruitmentData[] = [
     },
     dialog: {
       title: "Asisten Laboratorium",
-      description: "Kesempatan menjadi bagian dari tim laboratorium periode 2025 – 2026",
+      description:
+        "Kesempatan menjadi bagian dari tim laboratorium periode 2025 – 2026",
       content: (
         <>
           <div className="w-full text-base-foreground-400 text-base/7">
-            <h3 className="scroll-m-20 text-lg font-semibold tracking-tight text-base-foreground-200">Persyaratan</h3>
+            <h3 className="scroll-m-20 text-lg font-semibold tracking-tight text-base-foreground-200">
+              Persyaratan
+            </h3>
             <ul className="mt-3 mb-6 ml-6 list-disc [&>li]:mt-2">
               <li>Mahasiswa aktif semester 3 &ndash; 5.</li>
               <li>IPK minimal 3.00.</li>
@@ -44,7 +48,9 @@ export const recruitment: RecruitmentData[] = [
           </div>
 
           <div className="w-full text-base-foreground-400 text-base/7">
-            <h3 className="scroll-m-20 text-lg font-semibold tracking-tight text-base-foreground-200">Benefit</h3>
+            <h3 className="scroll-m-20 text-lg font-semibold tracking-tight text-base-foreground-200">
+              Benefit
+            </h3>
             <ul className="mt-3 mb-6 ml-6 list-disc [&>li]:mt-2">
               <li>Menambah value untuk pengalaman kerja.</li>
               <li>Kesempatan mengembangkan softskill dan hardskill.</li>
@@ -55,7 +61,9 @@ export const recruitment: RecruitmentData[] = [
           <Separator />
 
           <div>
-            <Button size="lg" disabled>Pendaftaran Ditutup</Button>
+            <Button size="lg" disabled>
+              Pendaftaran Ditutup
+            </Button>
           </div>
         </>
       ),
@@ -79,14 +87,20 @@ export default function Page() {
         className="grid grid-cols-1 gap-6 my-10 px-4 py-10 sm:px-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:px-8"
       >
         {recruitment.map((item, index) => (
-          <RecruitmentCard
-            key={index}
-            poster={item.poster}
-            name={item.name}
-            dateFrom={item.date.from}
-            dateTo={item.date.to}
-            dialog={item.dialog}
-          />
+          <BlurFade
+            key={item.name}
+            delay={0.45 + index * 0.15}
+            direction="up"
+            inView
+          >
+            <RecruitmentCard
+              poster={item.poster}
+              name={item.name}
+              dateFrom={item.date.from}
+              dateTo={item.date.to}
+              dialog={item.dialog}
+            />
+          </BlurFade>
         ))}
       </Container>
     </>
