f14c0af6695b8df70a80a43c98fdade7a4894065
Riz <ryzmdn@gmail.com>
2026-02-02 16:54:36 +0700

    feat(news-animate): added news page animation

--- app/(informasi)/berita-acara/page.tsx
+++ app/(informasi)/berita-acara/page.tsx
@@ -4,6 +4,7 @@ import { Card } from "@/components/ui/post-card";
 import { Container } from "@/components/container";
 import { SectionHeader } from "@/components/heading";
 import { SearchAlert } from "lucide-react";
+import { BlurFade } from "@/components/ui/blur-fade";
 
 async function getRecentPosts(): Promise<Post[]> {
   return await wordPressService.getPosts<Post>();
@@ -23,20 +24,33 @@ export default async function Page() {
 
       <Container as="section" className="px-4 mb-10 pb-16 sm:px-6 py-8 lg:px-8">
         {recentPosts.length === 0 ? (
-          <div className="text-center py-8 w-full space-y-6">
+          <BlurFade
+            delay={0.45}
+            direction="up"
+            inView
+            className="text-center py-8 w-full space-y-6"
+          >
             <SearchAlert className="size-20 text-base-foreground-300 mx-auto" />
-            <p className="text-base-foreground-400 font-medium">Belum ada berita acara</p>
-          </div>
+            <p className="text-base-foreground-400 font-medium">
+              Belum ada berita acara
+            </p>
+          </BlurFade>
         ) : (
           <div className="grid grid-cols-1 gap-x-5 gap-y-8 my-8 py-8 sm:grid-cols-2 lg:grid-cols-3">
-            {recentPosts.map((post) => (
-              <Card
+            {recentPosts.map((post, index) => (
+              <BlurFade
                 key={post.id}
-                featuredImage={post._embedded?.["wp:featuredmedia"]?.[0]}
-                title={post.title.rendered}
-                url={post.slug}
-                date={post.date}
-              />
+                delay={0.45 + index * 0.15}
+                direction="up"
+                inView
+              >
+                <Card
+                  featuredImage={post._embedded?.["wp:featuredmedia"]?.[0]}
+                  title={post.title.rendered}
+                  url={post.slug}
+                  date={post.date}
+                />
+              </BlurFade>
             ))}
           </div>
         )}
