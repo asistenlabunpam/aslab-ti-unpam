f9674f61e5e2fa4d5c7e2c912f5073617bb0bbc9
Riz <ryzmdn@gmail.com>
2026-02-02 14:43:02 +0700

    feat(about-adjust): adjusted the animation in the about section

--- components/home/about-section.tsx
+++ components/home/about-section.tsx
@@ -53,20 +53,21 @@ export function AboutSection() {
 
         <div className="lg:flex lg:flex-auto lg:justify-center">
           <dl className="flex flex-wrap items-center gap-x-10 gap-y-8 w-full lg:flex-col lg:items-start lg:w-64 xl:w-80">
-            {stats.map((stat) => (
-              <div
+            {stats.map((stat, index) => (
+              <BlurFade
                 key={stat.label}
+                delay={0.35 + index * 0.25} direction="left" inView
                 className="flex flex-col-reverse gap-y-3 pl-7 border-l border-base-300"
               >
                 <dt className="text-base/7 text-base-foreground-400">
-                  <TextAnimate delay={0.45} animation="slideLeft" by="character" once>
+                  <TextAnimate delay={0.35} animation="slideLeft" by="character" once>
                     {stat.label}
                   </TextAnimate>
                 </dt>
                 <dd className="text-5xl font-semibold tracking-tight text-base-foreground-200">
-                  <NumberTicker delay={0.25} value={stat.value} />
+                  <NumberTicker delay={0.65} value={stat.value} />
                 </dd>
-              </div>
+              </BlurFade>
             ))}
           </dl>
         </div>
@@ -76,28 +77,28 @@ export function AboutSection() {
         as="section"
         className="flow-root space-y-6 my-8 py-8 w-full max-w-3xl"
       >
-        <div className="bg-base-50 space-y-5 p-6 h-max border border-base-200 rounded-2xl">
-          <BlurFade delay={0.25} direction="up" inView>
+        <BlurFade delay={0.25} direction="up" inView className="bg-base-50 space-y-5 p-6 h-max border border-base-200 rounded-2xl">
+          <BlurFade delay={0.45} direction="up" inView>
             <h3 className="scroll-m-20 text-2xl text-base-foreground-200 font-semibold tracking-tight">
               {VISION_MISSION.vision.title}
             </h3>
           </BlurFade>
-          <BlurFade delay={0.45} direction="up" inView>
+          <BlurFade delay={0.55} direction="up" inView>
             <p className="text-base/7 text-base-foreground-400">
               {VISION_MISSION.vision.description}
             </p>
           </BlurFade>
-        </div>
+        </BlurFade>
 
-        <div className="bg-base-50 space-y-5 p-6 h-max border border-base-200 rounded-2xl">
-          <BlurFade delay={0.65} direction="up" inView>
+        <BlurFade delay={0.55} direction="up" inView className="bg-base-50 space-y-5 p-6 h-max border border-base-200 rounded-2xl">
+          <BlurFade delay={0.6} direction="up" inView>
             <h3 className="scroll-m-20 text-2xl text-base-foreground-200 font-semibold tracking-tight">
               {VISION_MISSION.mission.title}
             </h3>
           </BlurFade>
           <ul className="text-base/7 text-base-foreground-400 ml-6 [&>div]:mt-2">
             {VISION_MISSION.mission.items.map((item, index) => (
-              <BlurFade key={item} delay={0.85} direction="up" inView>
+              <BlurFade key={item} delay={0.7 + index * 0.15} direction="up" inView>
                 <li className="flex gap-x-3">
                   <span className="shrink-0 inline-flex justify-center items-center mt-1.5 size-4 rounded-full bg-static-50 text-[0.5rem] font-medium text-static-600 ring-1 ring-inset ring-static-500/10 dark:bg-static-400/10 dark:text-static-400 dark:ring-static-400/20">
                     {index + 1}
@@ -107,7 +108,7 @@ export function AboutSection() {
               </BlurFade>
             ))}
           </ul>
-        </div>
+        </BlurFade>
       </Container>
     </>
   );
